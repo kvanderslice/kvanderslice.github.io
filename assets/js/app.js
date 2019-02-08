231f3885f999c8534f7bceb2b5821acfa4945208
kvanderslice <k.vanderslice@yahoo.com>
2019-02-08 07:56:06 -0800

    layout revised

--- assets/js/app.js
+++ assets/js/app.js
@@ -957,5 +957,14 @@
     })
   }
 
+  /**
+   * Menu
+   */
+
+  const menuToggle = document.querySelector('.menu--toggle')
+  menuToggle.addEventListener('click', function () {
+    document.body.classList.toggle('menu--opened')
+  })
+
   return app
 })(jQuery)
